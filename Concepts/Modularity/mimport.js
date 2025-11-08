511b8cc54826700bcc7f41f1dbf459b075926a29
Nishant Singh <nsnishant@gmail.com>
2025-11-08 10:54:33 +0530

    cleanup

--- Concepts/Modularity/mimport.js
+++
@@ -1,9 +0,0 @@
-// ES6 format for importing
-// import { fullName, userName } from './jsModules.js'
-
-// import Default Export
-// import userData from './jsModules.js'
-
-// ES5 format for importing
-const userData = require('./mexport.js')
-console.log(userData);
